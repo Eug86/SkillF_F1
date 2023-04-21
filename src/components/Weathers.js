7ccf4024f9dc608bac2e4895bdb7e85d1953ae84
Eug86 <110021161+Eug86@users.noreply.github.com>
2023-04-21 10:02:18 +0500

    Update Weathers.js

--- src/components/Weathers.js
+++ src/components/Weathers.js
@@ -3,12 +3,12 @@ import axios from "axios";
 
 
 function Weathers({name}) {
-    let WeaUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + "Moscow" + ",ru&APPID=fe4c586fd2c32a2e3e13d3ad079f4ea1";
+    //let WeaUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + "Moscow" + ",ru&APPID=fe4c586fd2c32a2e3e13d3ad079f4ea1";
     //const [name, setName] = useState(0);
-    axios.get(WeaUrl).then(res => {
+    //axios.get(WeaUrl).then(res => {
         //console.log(res.data.name);
         //setName(res.data.name);
-    });
+    //});
     console.log({name});
     return (
         <h1>{name}</h1>
